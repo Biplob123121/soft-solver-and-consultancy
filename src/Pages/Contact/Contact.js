1439f4afca43d3e0d19f1b4cbd3d17419184354d
Biplob123121 <biplobkb123@gmail.com>
2023-09-02 14:18:49 +0600

    completed the contact section

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3qi8fl9', 'template_xwrtk3j', form.current, 'Cs-CMHdNv8wADgZ6K')
+      .then((result) => {
+          console.log(result.text);
+          console.log("message sent");
+          alert("Message Sent");
+          
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+
   return (
     <div className='py-16 px-12 bg-gray-900 bg-gradient-to-tr from-purple-800 to-green-800'>
       <h1 className='text-6xl font-bold text-center text-gray-100 mb-12'>Contact US</h1>
@@ -18,22 +36,22 @@ function Contact() {
           <p className='text-xl text-gray-900 mb-6'>zuhori@ualberta.ca</p>
         </div>
         <div className='bg-gray-200 px-6 py-12 rounded-xl shadow-xl'>
-        <form className='w-full'>
+        <form ref={form} onSubmit={sendEmail} className='w-full'>
             <div className="form-control">
               <label className="label"><span className="label-text">Name</span></label>
-              <input type="text" className="input input-bordered w-full"/>
+              <input type="text" name="user_name" className="input input-bordered w-full"/>
             </div>
             <div className="form-control">
               <label className="label"><span className="label-text">Email</span></label>
-              <input type="email" className="input input-bordered w-full" />
+              <input type="email" name="user_email" className="input input-bordered w-full" />
             </div>
             <div className="form-control">
               <label className="label"><span className="label-text">Phone Number</span></label>
-              <input type="email" className="input input-bordered w-full" />
+              <input type="text" name="user_phone" className="input input-bordered w-full" />
             </div>
             <div className="form-control">
               <label className="label"><span className="label-text">Message</span></label>
-              <textarea className="textarea textarea-bordered w-full"></textarea>
+              <textarea name="message" className="textarea textarea-bordered w-full" placeholder='Write which service do you want...'></textarea>
             </div>
             <div className='flex justify-center'>
               <input className='mt-6 bg-gradient-to-tr from-primary to-secondary px-16 md:px-32 py-3 rounded-lg hover:from-orange-500 hover:to-red-500 font-bold uppercase text-gray-500 shadow-lg' value="Send" type="submit" />
