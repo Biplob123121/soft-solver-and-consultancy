544cf0b990552e100219f963355d00e6dc147d83
stz-alberta <tauhid.ruet04@gmail.com>
2023-09-09 15:28:29 +0600

    updated the site

--- src/Pages/Career/Circular.js
+++ src/Pages/Career/Circular.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 function Circular() {
   return (
-    <div className='bg-gray-900 py-8 text-slate-100 flex flex-col justify-center items-center rounded-xl'>
+    <div className='bg-gray-900 py-8 text-slate-100 flex flex-col justify-center items-center'>
+      <div className='px-8 md:px-0'>
         <h1 className='text-4xl font-semibold my-16 text-center text-primary'>Present Circular</h1>
-        <p className='my-2 text-lg'>We are hiring expert developer..</p>
-        <p className='mb-1 text-lg'>To join our team you can send your CV or Resume in our Email.</p>
-        <h3 className='text-lg font-semibold'>Our Email : contact@ssconsult.ca</h3>
+        <p className='my-2 text-lg text-center'>We are hiring expert developer..</p>
+        <p className='mb-1 text-lg text-center'>To join our team you can send your CV or Resume in our Email.</p>
+        <h3 className='text-xl font-semibold text-center'>Our Email : contact@ssconsult.ca</h3>
+      </div>
     </div>
   )
 }
